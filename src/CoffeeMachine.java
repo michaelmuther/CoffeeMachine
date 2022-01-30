db1bea09fa7b7dbfdde4c86dcf40d50ce346f7ae
Michael Muther <michaelmuther@yahoo.com>
2022-01-30 14:38:07 +0100

    Initial commit

---
+++ src/CoffeeMachine.java
@@ -0,0 +1,20 @@
+
+public class CoffeeMachine {
+    final static String STARTING_STR = "Starting to make a coffee";
+    final static String GRINDING_STR = "Grinding coffee beans";
+    final static String BOILING_STR = "Boiling water";
+    final static String MIXING_STR = "Mixing boiled water with crushed coffee beans";
+    final static String POURING_COFFEE_STR = "Pouring coffee into the cup";
+    final static String POURING_MILK_STR = "Pouring some milk into the cup";
+    final static String READY_STR = "Coffee is ready!";
+
+    public static void main(String[] args) {
+        System.out.println(STARTING_STR);
+        System.out.println(GRINDING_STR);
+        System.out.println(BOILING_STR);
+        System.out.println(MIXING_STR);
+        System.out.println(POURING_COFFEE_STR);
+        System.out.println(POURING_MILK_STR);
+        System.out.println(READY_STR);
+    }
+}
